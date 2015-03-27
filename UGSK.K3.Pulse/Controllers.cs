86be1a3e9487d46b6e69f45d0168a417ac46af1d
IvanovAA <ivanovaa@ugsk.ru>
2015-03-27 08:42:22 +0500

    Add products controller

--- UGSK.K3.Pulse/Controllers.cs
+++ UGSK.K3.Pulse/Controllers.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Hangfire;
+using System.Collections.Generic;
 
 namespace UGSK.K3.Pulse
 {
@@ -13,10 +14,10 @@ namespace UGSK.K3.Pulse
         public CounterController(ICounterQuery counterQuery)
         {
             _counterQuery = counterQuery;
-            
+
         }
 
-        public async Task<Counter> Get(string product, PeriodKind periodKind = PeriodKind.Daily, DateTimeOffset? periodStart = null, CounterKind counterKind=CounterKind.Total)
+        public async Task<Counter> Get(string product, PeriodKind periodKind = PeriodKind.Daily, DateTimeOffset? periodStart = null, CounterKind counterKind = CounterKind.Total)
         {
             return await _counterQuery.GetCounter(product, periodKind, periodStart, counterKind);
         }
@@ -25,7 +26,7 @@ namespace UGSK.K3.Pulse
         {
             // run asynchronously to leave web request thread
             BackgroundJob.Enqueue<DefaultCounterProcessorAdapter>(p => p.Process(notification));
-            
+
             return StatusCode(HttpStatusCode.Accepted);
         }
     }
@@ -51,6 +52,21 @@ namespace UGSK.K3.Pulse
             await _processor.Process(index);
 
             return StatusCode(HttpStatusCode.Accepted);
+        }
+    }
+
+    public class ProductsController : ApiController
+    {
+        private readonly IDataStorage _dataStorage;
+        public ProductsController(IDataStorage dataStorage)
+        {
+            _dataStorage = dataStorage;
+        }
+
+        public async Task<IEnumerable<string>> Get()
+        {
+            var products = await _dataStorage.GetProducts();
+            return products;
         }
     }
 }
